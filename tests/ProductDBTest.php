e588854a869ddc24c02d3469697bd85bdba4f1f2
ATCamposs <andre.tatibano@gmail.com>
2020-09-21 13:56:26 -0300

    Gerando testes dependentes do resultado de outros.

--- tests/ProductDBTest.php
+++ tests/ProductDBTest.php
@@ -22,6 +22,7 @@ class ProductDBTest extends PHPUnit\Framework\TestCase
         $this->assertEquals(200.20, $result->getPrice());
         $this->assertEquals(10, $result->getQuantity());
         $this->assertEquals(200.20*10, $result->getTotal());
+        return $result->getId();
     }
 
     public function testIfListProducts()
@@ -35,5 +36,26 @@ class ProductDBTest extends PHPUnit\Framework\TestCase
         ]);
         $products = $product->all();
         $this->assertCount(2, $products);
+    }
+
+    /**
+     * @depends testIfProductIsSaved
+     */
+    public function testIfProductIsUpdated($id)
+    {
+        global $db;
+        $product = new Product($db);
+        $result = $product->save([
+            'id' => $id,
+            'name' => 'Produto Alterado',
+            'price' => 300.20,
+            'quantity' => 20
+        ]);
+
+        $this->assertEquals($id, $result->getId());
+        $this->assertEquals('Produto Alterado', $result->getName());
+        $this->assertEquals(300.20, $result->getPrice());
+        $this->assertEquals(20, $result->getQuantity());
+        $this->assertEquals(300.20*20, $result->getTotal());
     }
 }
